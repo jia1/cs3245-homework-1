cadad39f19d3f4121bcc7b45139b78533ac54230
JIA YEE <jiayeerawr@gmail.com>
2018-01-29 15:37:39 +0800

    Fix syntax errors, typo, and add setup files
    Signed-off-by: JIA YEE <jiayeerawr@gmail.com>

--- build_test_LM.py
+++ build_test_LM.py
@@ -11,7 +11,7 @@ def build_LM(in_file):
     build language models for each label
     each line in in_file contains a label and an URL separated by a tab(\t)
     """
-    print 'building language models...'
+    print('building language models...')
     # This is an empty method
     # Pls implement your code in below
     labels_indexer = {} # indexer[label] = label_index
@@ -22,21 +22,24 @@ def build_LM(in_file):
     with open(in_file) as f:
         for line in f:
             label, sentence = line.strip().split(' ', 1)
-            if label not in labels:
-                labels[label] = len(model)
+            if label not in labels_indexer:
+                labels_indexer[label] = len(models)
                 ngrams_counter[label] = {}
-                model.append([])
+                models.append([])
             for i in range(len(sentence) - n):
                 ngram = tuple([ch.lower() for ch in sentence[i:i+n]])
-                if ngram not in ngrams:
+                if ngram not in ngrams_indexer:
                     ngrams_indexer[ngram] = ngrams_next_index
                     ngrams_next_index += 1
                     ngrams_counter[label][ngram] = 1
                 else:
-                    ngrams_counter[label][ngram] += 1
-        for label, label_index in labels.items():
+                    if ngram not in ngrams_counter[label]:
+                        ngrams_counter[label][ngram] = 1
+                    else:
+                        ngrams_counter[label][ngram] += 1
+        for label, label_index in labels_indexer.items():
             models[label_index] = [1 for ngram in ngrams_indexer]
-            for ngram, ngram_count in ngrams_table[label]:
+            for ngram, ngram_count in ngrams_counter[label].items():
                 ngram_index = ngrams_indexer[ngram]
                 models[label_index][ngram_index] = ngram_count
             models[label_index] = [ngram_count / len(ngrams_indexer) for ngram_count in models[label_index]]
@@ -48,7 +51,7 @@ def test_LM(in_file, out_file, LM):
     each line of in_file contains an URL
     you should print the most probable label for each URL into out_file
     """
-    print "testing language models..."
+    print('testing language models...')
     # This is an empty method
     # Pls implement your code in below
     labels_indexer, ngrams_indexer, models = LM
@@ -62,19 +65,19 @@ def test_LM(in_file, out_file, LM):
                     if ngram not in ngrams_indexer:
                         continue
                     ngram_index = ngrams_indexer[ngram]
-                    for label, label_index in labels_indexer:
-                        ngram_probability = models[labels_indexer][ngram_index]
+                    for label, label_index in labels_indexer.items():
+                        ngram_probability = models[label_index][ngram_index]
                         probabilities[label] *= ngram_probability
                 label = max(probabilities, key=probabilities.get)
-                g.write('{label} {sentence}'.format(label, line))
+                g.write('{label} {sentence}'.format(label=label, sentence=line))
 
 def usage():
-    print "usage: " + sys.argv[0] + " -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file"
+    print('usage: ' + sys.argv[0] + ' -b input-file-for-building-LM -t input-file-for-testing-LM -o output-file')
 
 input_file_b = input_file_t = output_file = None
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'b:t:o:')
-except getopt.GetoptError, err:
+except (getopt.GetoptError, err) as e:
     usage()
     sys.exit(2)
 for o, a in opts:
